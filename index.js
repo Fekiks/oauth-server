3f9a7321eea4b1ae28601bbddeca23d1f48099ff
Felix <86270566+Fekiks@users.noreply.github.com>
2026-04-03 00:28:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -85,3 +85,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("OAuth server running on port", PORT);
 });
+
+setInterval(() => {
+  console.log("still alive");
+}, 10000);
